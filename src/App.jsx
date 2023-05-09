4d0a38b4f899359b3e72faca70b979445c7cacf6
paresiqbal <thecrowdel@gmail.com>
2023-05-09 23:44:29 +0700

    Add Error page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // route
 import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />,
+    errorElement: <Error />,
   },
 ]);
 
