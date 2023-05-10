320076101b5f8de4651e04bcec7f0f14467b1690
paresiqbal <thecrowdel@gmail.com>
2023-05-10 14:30:28 +0700

    Create Route, Navbar, And laoyouts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // route
+import MainLayout from "./layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        errorElement: <Error />,
+      },
+    ],
   },
 ]);
 
