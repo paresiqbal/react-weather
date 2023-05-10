0587bc0157a859f62e1b92c7f01da99d65d6896d
paresiqbal <thecrowdel@gmail.com>
2023-05-10 14:58:00 +0700

    Finishing Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,36 @@
+// react
+import { NavLink } from "react-router-dom";
+
+// library
+import {
+  SunIcon,
+  InformationCircleIcon,
+  PlusIcon,
+} from "@heroicons/react/24/solid";
+
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="top-0 sticky shadow-lg bg-weather-primary">
+      <div className="container flex flex-col sm:flex-row items-center gap-4 text-white py-6 ">
+        <NavLink
+          to={"/"}
+          aria-label="HomePage"
+          className="flex items-center gap-2"
+        >
+          <SunIcon width={20} />
+          <p className="text-xl font-semibold">Local Weather</p>
+        </NavLink>
+        <div className="flex gap-2 flex-1 justify-end">
+          <InformationCircleIcon
+            width={20}
+            className="hover:text-weather-secondary duration-150 cursor-pointer"
+          />
+          <PlusIcon
+            width={20}
+            className="hover:text-weather-secondary duration-150 cursor-pointer"
+          />
+        </div>
+      </div>
     </nav>
   );
 }
