11d72cae17af407e2795edeeb73944107e4ec7f9
paresiqbal <thecrowdel@gmail.com>
2023-05-11 23:30:17 +0700

    Add AsyncCityView

---
+++ src/pages/AsyncCityView.jsx
@@ -0,0 +1,30 @@
+// library
+import axios from "axios";
+
+export default function AsyncCityView() {
+  const getWeatherData = async () => {
+    try {
+      const weatherData = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=b1fe8bb6a22e846d16d17e5c31fe22c5&units=imperial`
+      );
+
+      // cal current date & time
+      const localOffset = new Date().getTimezoneOffset() * 60000;
+      const utc = weatherData.data.current.dt * 1000 + localOffset;
+      weatherData.data.currentTime =
+        utc + 1000 * weatherData.data.timezone_offset;
+
+      // cal hourly weather offset
+      weatherData.data.hourly.forEach((hour) => {
+        const utc = hour.dt * 1000 + localOffset;
+        hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+      });
+
+      return weatherData.data;
+    } catch (error) {
+      console.log(error);
+    }
+
+    const weatherData = await getWeatherData();
+  };
+}
