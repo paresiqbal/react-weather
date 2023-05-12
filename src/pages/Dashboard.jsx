00565aa77f17dffcd35cc828412e05b0dc528add
paresiqbal <thecrowdel@gmail.com>
2023-05-12 22:41:20 +0700

    Adding new way to make weather Apps

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,56 +1,4 @@
-// react
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-// library
-import axios from "axios";
-
 export default function Dashboard() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [queryTimeout, setQueryTimeout] = useState(null);
-
-  const navigate = useNavigate();
-
-  const [mapboxSearchResults, setMapboxSearchResults] = useState(null);
-  const mapboxAPIKey =
-    "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
-
-  const previewCity = (searchResult) => {
-    const [city, state] = searchResult.place_name.split(",");
-    navigate(`/city/${state.replaceAll(" ", "")}/${city}`, {
-      state: {
-        name: "cityView",
-        params: { state: state, city: city },
-        search: `?lat=${searchResult.geometry.coordinates[1]}&lng=${searchResult.geometry.coordinates[0]}&preview=true`,
-      },
-    });
-  };
-
-  const getSearchResults = () => {
-    clearTimeout(queryTimeout);
-    setQueryTimeout(
-      setTimeout(async () => {
-        if (searchQuery !== "") {
-          try {
-            const result = await axios.get(
-              `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery}.json?access_token=${mapboxAPIKey}&types=place`
-            );
-            setMapboxSearchResults(result.data.features);
-          } catch (error) {
-            setError = true;
-          }
-
-          return;
-        }
-        setMapboxSearchResults(null);
-      }, 300)
-    );
-  };
-
-  useEffect(() => {
-    getSearchResults();
-  }, [searchQuery]);
-
   return (
     <main className="container text-white">
       <div className="pt-4 mb-8 relative">
@@ -61,19 +9,6 @@ export default function Dashboard() {
           placeholder="Search for a city or state"
           className="py-2 px-1 w-full bg-weather-secondary border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
         />
-        {mapboxSearchResults && (
-          <ul className="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
-            {mapboxSearchResults.map((searchResult) => (
-              <li
-                key={searchResult.id}
-                className="py-2 cursor-pointer"
-                onClick={() => previewCity(searchResult)}
-              >
-                {searchResult.place_name}
-              </li>
-            ))}
-          </ul>
-        )}
       </div>
     </main>
   );
