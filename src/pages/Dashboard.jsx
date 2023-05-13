0893882669ede5859e491a04d38c344bcd57e8cd
paresiqbal <thecrowdel@gmail.com>
2023-05-13 23:28:56 +0700

    Add input tags

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
 export default function Dashboard() {
   return (
     <main className="container text-white">
-      <div className="pt-4 mb-8 relative"></div>
+      <div className="pt-4 mb-8 relative">
+        <input
+          type="text"
+          placeholder="Search for a city"
+          className="py-2 px-1 w-full bg-weather-secondary border-b rounded-sm focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+        />
+      </div>
     </main>
   );
 }
